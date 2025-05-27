277ed990d9326d38d83b2df69b054e42fb0bf070
Ash19-88 <ayelenllampa88@gmail.com>
2025-05-27 15:21:46 -0300

    feat: cart changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,70 @@
-
-import Header from './components/header/Header'
-import Nav from './components/navBar/Nav'
-import { Routes, Route } from 'react-router-dom';
+import Header from "./components/header/Header";
+import Nav from "./components/navBar/Nav";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from './components/home/Home';
-import About from './components/about/About';
-import Shop from './components/shop/Shop';
-import Contact from './components/contact/Contact';
-import Cart from './components/cart/Cart';
-import Footer from './components/footer/Footer';
-import { useEffect, useState } from 'react';
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Shop from "./components/shop/Shop";
+import Contact from "./components/contact/Contact";
+import Cart from "./components/cart/Cart";
+import Footer from "./components/footer/Footer";
+import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import ProductDetails from "./components/product/ProductDetails";
 
 function App() {
-
-  const [items,setItems]= useState(() => {
-    const storedItems = localStorage.getItem('cartItems');
+  const [items, setItems] = useState(() => {
+    const storedItems = localStorage.getItem("cartItems");
     return storedItems ? JSON.parse(storedItems) : [];
-  })
+  });
 
   useEffect(() => {
-    localStorage.setItem('cartItems', JSON.stringify(items));
+    localStorage.setItem("cartItems", JSON.stringify(items));
   }, [items]);
 
   const addToCart = (item) => {
     const existingItem = items.find((i) => i.id === item.id);
     if (existingItem) {
-      setItems(items.map((i) => (i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i)));
+      setItems(
+        items.map((i) =>
+          i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i
+        )
+      );
     } else {
       setItems([...items, { ...item, quantity: 1 }]);
     }
-  }
+  };
 
   return (
-      <>
-        <Header />
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/shop" element={<Shop  addToCart={addToCart} />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/cart" element={<Cart  items={items} setItems={setItems}/>} />
-        </Routes>
-        <Footer/>
-      </>
-  )
+    <>
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop addToCart={addToCart} />} />
+        <Route path="/product/:id" element={<ProductDetails addToCart={addToCart} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route
+          path="/cart"
+          element={<Cart items={items} setItems={setItems} />}
+        />
+      </Routes>
+      <Footer />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./Cart.module.css";
+import { BsTrash3 } from "react-icons/bs";
+import { RiArrowRightSLine } from "react-icons/ri";
 
 const Cart = ({ items, setItems }) => {
   const total = items.reduce(
@@ -13,43 +15,55 @@ const Cart = ({ items, setItems }) => {
     localStorage.setItem("cartItems", JSON.stringify(updatedCart));
   };
 
+
   return (
     <div className={styles.mainCartContainer}>
+        <Link to="/shop" className={styles.keepShoppingButton}>
+              <i className="fas fa-chevron-left"></i>
+              Seguir Comprando <RiArrowRightSLine />
+            </Link>
       <div className="container text-center my-5">
+        <h1 className={styles.cartTitle}>Tu carrito de compras</h1>
         <section id="cart" className={styles.cartContainer}>
           <table className="table">
             <thead>
               <tr>
-                <th scope="col">Producto</th>
-                <th scope="col">Precio</th>
-                <th scope="col">Cantidad</th>
-                <th scope="col">Total</th>
-                <th scope="col"></th>
+                <th className={styles.tableHead} scope="col">Producto</th>
+                <th className={styles.tableHead} scope="col">Precio</th>
+                <th className={styles.tableHead} scope="col">Cantidad</th>
+                <th className={styles.tableHead} scope="col">Subtotal</th>
+                <th className={styles.tableHead} scope="col"></th>
               </tr>
             </thead>
+            <tbody>
             {items.map((item, index) => (
-              <tr key={index}>
-                <td>{item.title}</td>
-                <td>${item.price}</td>
-                <td>{item.quantity || 1}</td>
-                <td>${item.price * (item.quantity || 1)}</td>
-                <td>
-                  <button onClick={() => clearCart(item.id)}>Eliminar</button>
+              <tr key={index} className={styles.cartItem}>
+                <td className={styles.productCart}>
+                    <img
+                      src={item.image}
+                      className={`d-block w-100 ${styles.productCartImage}`}
+                      alt={item.title}
+                    />
+                    <h2 className={styles.productCartTitle}>{item.title}</h2>  
+                </td>
+                <td className={styles.tableData}>${item.price}</td>
+                <td className={styles.tableData}>{item.quantity || 1}</td>
+                <td className={styles.tableData}>${item.price * (item.quantity || 1)}</td>
+                <td className={styles.tableData}>
+                  <button onClick={() => clearCart(item.id)} className={styles.trashButton}><BsTrash3 size={18}/></button>
                 </td>
               </tr>
             ))}
+            </tbody>
           </table>
           <h3 className={styles.totalBox}>
             Total: $<span id="cart-total">{total.toFixed(2)}</span>
           </h3>
           <div className={styles.cartSummary}>
             <button id="checkout-button" className={styles.checkoutButton}>
-              Pagar
+              Pagar ahora ({items.length})
             </button>
-            <Link to="/shop" className={styles.keepShoppingButton}>
-              <i className="fas fa-chevron-left"></i>
-              Seguir Comprando
-            </Link>
+            
           </div>
         </section>
       </div>
